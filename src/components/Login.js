ff5e183d101bb6d735ceffb902d5836d3844cf39
Patel <vishal.c.patel@capgemini.com>
2019-05-30 17:46:16 +0530

    login success msg

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,8 @@ class Login extends React.Component {
 		console.log(userStore.getState());
 		this['state'] = {
 			username: '',
-			password: ''
+			password: '',
+			loggedInUserData: null
 		}
 	}
 	getUsername = (e) => {
@@ -16,11 +17,21 @@ class Login extends React.Component {
 		this['setState'](() => {
 			return { username };
 		});
+		this['setState'](() => {
+			return {
+				loggedInUserData: false
+			};
+		});
 	}
 	getPassword = (e) => {
 		let password = e.target.value;
 		this['setState'](() => {
 			return { password };
+		});
+		this['setState'](() => {
+			return {
+				loggedInUserData: false
+			};
 		});
 	}
 	onLogin = (e) => {
@@ -28,12 +39,19 @@ class Login extends React.Component {
 			username: this['state'].username,
 			password: this['state'].password
 		}
+		this['setState'](() => {
+			return {
+				loggedInUserData: this.validateUser(data)
+			};
+		});
 		this.validateUser(data);
 	}
 	validateUser(data) {
 		return userStore.getState().filter((e) => {
 			if ((e.username === data.username) && (e.password === data.password)) {
 				return true;
+			} else {
+				return false;
 			}
 		});
 	}
@@ -44,6 +62,9 @@ class Login extends React.Component {
 				<div><input type="text" placeholder="Username" value={this['state'].username} onChange={this.getUsername} /></div>
 				<div><input type="text" placeholder="Password" value={this['state'].password} onChange={this.getPassword} /></div>
 				<br />
+				{(this['state'].loggedInUserData && this['state'].loggedInUserData !== null && this['state'].loggedInUserData.length !== 1) ? 'Username or password invalid' : null}
+				{(this['state'].loggedInUserData && this['state'].loggedInUserData.length === 1) ? 'Login successfully' : null}
+				<br /><br />
 				<div><button onClick={this.onLogin}>Login</button></div>
 				<br />
 				<Link to="/signup">Sign-up</Link>
